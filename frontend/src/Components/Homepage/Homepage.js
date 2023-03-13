53b5b9ec635ae6956562805b93c0c0d7368e399e
shorlo <javier@Hermes.local>
2023-03-13 14:45:13 +0100

    secord update

--- frontend/src/Components/Homepage/Homepage.js
+++ frontend/src/Components/Homepage/Homepage.js
@@ -6,26 +6,38 @@ import HomeImage from "../../assets/images/twitter_home.png";
 const Homepage = () =>
 {
     return(
-        <div className="home">
-            <div className="home_left--img">
-                <img className="home_left" src={HomeImage} alt="twitter_home" />
-            </div>
-            <div className="home_right">
-                <img className="logo" src={Logo} alt="logo"/>
-                <h1 className="home_right--title" >Happening now</h1>
-                <h2 className="home_right--subtitle">Join Twitter today.</h2>
-                <div className="home_right--auth__buttons">
-                    <button>Sign up with Google</button>
-                    <button>Sign up with Apple</button>
-                    <button>Sign up with phone or email</button>
-                    <p className="policies">
-                        By signing up, you agree to the <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>, including <a href="#">Cookies</a> Use.
-                    </p>
+        <div className="homepage">
+            <div className="homepage__top">
+                <div className="homepage__topleft">
+                    <img src={HomeImage} alt="twitter_home" />
+                </div>
+
+                <div className="homepage__top-right">
+                    <img className="logo" src={Logo} alt="logo"/>
+                    <h1 className="homepage__top-right--title" >Happening now</h1>
+                    <h2 className="homepage__top-right--subtitle">Join Twitter today.</h2>
+
+                    <div className="homepage_top-right--btns">
+                        <button className="btn_primary">Sign up with Google</button>
+                        <button className="btn_primary">Sign up with Apple</button>
+                        <button className="btn_secondary">Sign up with phone or email</button>
+
+                        <p className="homepage__right--policies">
+                            By signing up, you agree to the <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>, including{" "} <a href="#">Cookies</a> Use.
+                        </p>
+                    </div>
+
+                    <div className="homepage_top-right--btns">
+                        <h3>Already have an account?</h3>
+                        <button className="btn_primary">Sign in</button>
+                    </div>
                 </div>
             </div>
+            <footer className="footer">
+                <a href="#">Terms of Service</a><a href="#">Terms of Service</a><a href="#">Terms of Service</a><a href="#">Terms of Service</a><a href="#">Terms of Service</a><a href="#">Terms of Service</a><a href="#">Terms of Service</a><a href="#">Terms of Service</a><a href="#">Terms of Service</a><a href="#">Terms of Service</a><a href="#">Terms of Service</a><a href="#">Terms of Service</a><a href="#">Terms of Service</a><a href="#">Terms of Service</a><a href="#">Terms of Service</a><a href="#">Terms of Service</a>
+            </footer>
         </div>
     );
 };
 
 export default Homepage;
-//17:39
