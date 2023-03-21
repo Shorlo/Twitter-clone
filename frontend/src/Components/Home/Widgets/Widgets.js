345d7cb37689129060fbc18e68c2ce4065f748bf
Shorlo <shorlo.sbg@gmail.com>
2023-03-21 23:18:12 +0100

    update widgets view

--- frontend/src/Components/Home/Widgets/Widgets.js
+++ frontend/src/Components/Home/Widgets/Widgets.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import "./Widgets.css";
+import {TwitterTimelineEmbed} from "react-twitter-embed";
 
 const Widgets = () =>
 {
     return(
         <div className="widgets">
-            Widgets
+            <TwitterTimelineEmbed className="widgets__container" sourceType="profile" screenName="saurabhnemade" options={{height: 600}} />
         </div>
     );
 };
