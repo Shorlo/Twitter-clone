b72a8c4ab4d498700db794a25e189306cebe8243
Shorlo <shorlo.sbg@gmail.com>
2023-03-22 17:22:02 +0100

    Apollo v3 deprecated trying v4

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
 require('dotenv').config();
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const fs = require('fs');
+
+//const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const logger = require('morgan');
+
+
+
+
 const app = express();
 
 const port = process.env.PORT || 3001;
@@ -18,10 +28,29 @@ const corsOptions =
     credential: true,
     exposeHeaders: ["X-auth-token"],
 };
-
-app.use(cors(corsOptions));
-
-app.listen(port, ()=>
+/*
+app.listen(port, () =>
 {
     console.log(`Server running at ${port}`);
 });
+*/
+app.use(cors(corsOptions));
+
+// Using graphql
+const typeDefs = gql(fs.readFileSync("./schema.graphql", {encoding: "utf-8"}));
+const resolvers = require("./resolvers");
+const apolloServer = new ApolloServer({typeDefs, resolvers});
+const { url } = await startStandaloneServer(apolloServer,
+    {
+        listen: {port: 3001},
+    });
+    console.log(`Server ready at ${url}`);
+
+
+/*
+    Deprecated
+apolloServer.applyMiddleware({app, path: "/graphql"});
+*/
+
+
+/*ApolloServer v3 es deprecated, trying to migrate to v4*/
---
+++ backend/resolvers.js
@@ -0,0 +1,30 @@
+const register = async (_, input) =>
+{
+    const {firstName, lastName, email, password} = input;
+    if(firstName || lastName || email || password)
+    {
+        return "Field's required!";
+    }
+
+    try
+    {
+        const user = new User
+        ({
+            id:nanoid(),
+            firstName,
+            lastName,
+            email,
+            password,
+        });
+
+        await user.save();
+        return "User registered!";
+    }
+    catch(error)
+    {
+        return "Error occurred!!"
+    }
+
+};
+
+module.exports = {register, }
